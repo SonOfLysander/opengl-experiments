123b1f9d087541569e71dbc300f3d791f8c3e70e
Paul Baker <paul.baker@overstock.com>
2015-06-23 17:22:05 -0600

    Implemented initGL()

--- lazyfoo/lesson1/LUtil.cpp
+++ lazyfoo/lesson1/LUtil.cpp
@@ -6,4 +6,16 @@ bool initGL()
 	// Initialize Modelview Matrix
 	glMatrixMode(GL_MODELVIEW);
 	glLoadIdentity();
+
+	// Initialize clear color
+	glClearColor(0.f, 0.f, 0.f, 1.f);
+
+	// Check for errors
+	GLenum error = glGetError();
+	if (error != GL_NO_ERROR)
+	{
+		printf("Error initializing OpenFL %s\n", gluErrorString(error));
+		return false;
+	}
+	return true;
 }
