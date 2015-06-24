58cef99f2d54751964dfc5c97b8e499af88bb1aa
Paul Baker <paul.baker@overstock.com>
2015-06-23 19:16:13 -0600

    compile script and makefile are cleaned and reference ./bin for output

--- lazyfoo/lesson1/compile-example.sh
+++ lazyfoo/lesson1/compile-example.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-#g++ LOpenGL.h LUtil.h LUtil.cpp main.cpp -lglut -lGL -lGLU -lGLEW -lm -L/usr/include/GL
-g++ LUtil.cpp main.cpp -w -lGL -lGLU -lglut -o 01_hello_freeglut
+mkdir -p ./bin
+g++ LUtil.cpp main.cpp -w -lGL -lGLU -lglut -o ./bin/application.out
