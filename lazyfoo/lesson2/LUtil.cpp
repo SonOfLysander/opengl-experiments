ca448fb78edcd258ebbf2d4303732ebe73f5f860
Paul Baker <paul.baker@overstock.com>
2015-06-29 16:17:51 -0600

    Updated from tutorial. Now zooms and toggles colors

--- lazyfoo/lesson2/LUtil.cpp
+++ lazyfoo/lesson2/LUtil.cpp
@@ -37,14 +37,36 @@ void render()
 {
 	// Clear color buffer
 	glClear(GL_COLOR_BUFFER_BIT);
-	// Render the quad
-	glBegin(GL_QUADS);
-	float halfLength = 0.5f;
-	glVertex2f(-halfLength, -halfLength);
-	glVertex2f(halfLength, -halfLength);
-	glVertex2f(halfLength, halfLength);
-	glVertex2f(-halfLength, halfLength);
-	glEnd();
+	// Reset modelview matrix
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+	// Move to center of screen
+	glTranslate(SCREEN_WIDTH / 2.f, SCREEN_HEIGHT / 2.f, 0.f);
+	// Render quad
+	if (gColorMode == COLOR_MODE_CYAN)
+	{
+		// Solid Cyan
+		glBegin(GL_QUADS);
+		glColor3f(0.f, 1.f, 1.f);
+		glVertex2f(-50.f, -50.f);
+		glVertex2f(50.f, -50.f);
+		glVertex2f(50.f, 50.f);
+		glVertex2f(-50.f, 50.f);
+		glEnd();
+	}
+	else
+	{
+		glBegin(GL_QUADS);
+		glColor3f(1.f, 0.f, 0.f);
+		glVertex2f( -50.f, -50.f );
+		glColor3f( 1.f, 1.f, 0.f );
+		glVertex2f(  50.f, -50.f );
+		glColor3f( 0.f, 1.f, 0.f );
+		glVertex2f(  50.f,  50.f );
+		glColor3f( 0.f, 0.f, 1.f );
+		glVertex2f( -50.f,  50.f );
+		glEnd();
+	}
 	// Update screen
 	glutSwapBuffers();
 }
@@ -57,4 +79,43 @@ void runMainLoop(int val)
 
 	// Run frame one more time
 	glutTimerFunc(1000 / SCREEN_FPS, runMainLoop, val);
+}
+
+void handleKeys(unsigned char key, int x, int y)
+{
+	// If the user presses 'q'
+	if (key == 'q')
+	{
+		// Toggle color mode
+		if (gColorMode == COLOR_MODE_CYAN)
+		{
+			gColorMode = COLOR_MODE_MULTI;
+		}
+		else
+		{
+			gColorMode = COLOR_MODE_CYAN;
+		}
+	}
+	else if (key == 'e')
+	{
+		if (gProjectionScale == 1.f)
+		{
+			// Zoom out
+			gProjectionScale = 2.f;
+		}
+		else if (gProjectionScale == 2.f)
+		{
+			// Zoom in
+			gProjectionScale = 0.5f;
+		}
+		else if (gProjectionScale == 0.5f)
+		{
+			// Regular Zoom
+			gProjectionScale = 1.f;
+		}
+		// Update projection matrix
+		glMatrixMode(GL_PROJECTION);
+		glLoadIdentity();
+		glOrtho(0.0, SCREEN_WIDTH * gProjectionScale, SCREEN_HEIGHT * gProjectionScale, 0.0, 1.0, -1.0);
+	}
 }
